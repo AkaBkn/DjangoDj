d699176394d575166197be6ef0921a3164d3ef4f
Dominic Fischer <pirate.owns@gmail.com>
2010-09-02 01:35:33 +0200

    refactered, icon added, settings.ini commented

--- djdj.py
+++ djdj.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
 import gtk
 import appindicator
 import subprocess
@@ -5,46 +8,58 @@ import os
 import signal
 from ConfigParser import ConfigParser
 import logging
+import sys
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 SETTINGS_FILE = 'settings.ini'
+ICON = 'djdj-icon'
+ICON_ATTENTION = 'djdj-icon-attention'
+
+CUSTOM_COMMANDS_KEY = 'commands'
+SETTINGS_KEY = 'general'
 
 class DjDj(object):
     server = None
-    custom_commands = []
-    custom_runserver_command = ''
+
+    settings = {
+        'custom_runserver_command': None,
+        'django_project_path': None,
+        'commands': [],
+    }
 
     def __init__(self):
         self.read_settings()
 
+        if not self.settings['django_project_path']:
+            print >> sys.stderr, 'You need at least the \'django_project_path\' option set in your %s file!' % SETTINGS_FILE
+            sys.exit()
+
         self.orig_path = os.getcwd()
 
-        ind = appindicator.Indicator ("DjangDj", "djdj-icon", appindicator.CATEGORY_APPLICATION_STATUS)
+        ind = appindicator.Indicator ("DjangDj", ICON, appindicator.CATEGORY_APPLICATION_STATUS)
         
         ind.set_status (appindicator.STATUS_ACTIVE)
-        #ind.set_attention_icon ("indicator-messages-new")
+        #ind.set_attention_icon (ICON_ATTENTION)
 
         ind.set_menu(self.create_menu())
         
         gtk.main()
 
     def read_settings(self):
-        #f = open()
         c = ConfigParser()
         c.read(SETTINGS_FILE)
 
         for section in c.sections():
             for option in c.options(section):
-                if section == 'general':
-                    if option == 'django_project_path':
-                        self.django_path = c.get(section, option)
-                    elif option == 'custom_runserver_command':
-                        self.custom_runserver_command = c.get(section, option)
+                if section == SETTINGS_KEY:
+                    if option in self.settings and option is not CUSTOM_COMMANDS_KEY:
+                        self.settings[option] = c.get(section, option)
+                    else: continue
                         
-                elif section == 'commands':
-                    self.custom_commands.append((option, c.get(section, option)))
+                elif section == CUSTOM_COMMANDS_KEY:
+                    self.settings[CUSTOM_COMMANDS_KEY].append((option, c.get(section, option)))
 
         
 
@@ -60,9 +75,9 @@ class DjDj(object):
             ['Exit', self.exit],
         ]
 
-        for m in self.custom_commands:
+        for m in self.settings['commands']:
             menu_item = gtk.MenuItem(m[0])
-            menu_item.connect("activate", self.run_management_command, m[1])
+            menu_item.connect("activate", self.run_command, m[1])
             menu_item.show()
             gtk_menu.append(menu_item)
 
@@ -80,10 +95,10 @@ class DjDj(object):
 
     def run_server(self, item):
         if not self.server:
-            os.chdir(self.django_path)
+            os.chdir(self.settings['django_project_path'])
 
-            if self.custom_runserver_command:
-                cmd = self.custom_runserver_command.split()
+            if self.self.settings['custom_runserver_command']:
+                cmd = self.settings['custom_runserver_command'].split()
             else:
                 cmd = ['./manage.py', 'runserver']
 
@@ -100,9 +115,9 @@ class DjDj(object):
             self.server = None
             item.set_label('Run Server')
 
-    def run_management_command(self, item, command):
-        os.chdir(self.django_path)
-        subprocess.Popen(['./manage.py', command ])
+    def run_command(self, item, command):
+        os.chdir(self.settings['django_project_path'])
+        subprocess.Popen(command.split())
         os.chdir(self.orig_path)
 
     def about(self, item):
